f635c44c819084ac9a3ab51c48d9dab6e885ba1f
Nelson, Robert <rnelson0@gmail.com>
2014-04-20 20:47:26 +0000

    Add hiera to puppetmaster

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,12 @@ node 'puppet.nelson.va' {
   include ::base
   notify { "Generated from our notify branch": }
 
+  # PuppetDB
   include ::puppetdb
   include ::puppetdb::master::config
+
+  # Hiera
+  package { ['hiera', 'hiera-puppet']:
+    ensure => present,
+  }
 }
