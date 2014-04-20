0e599ef152d1e2e84573162646edeaa0971ca3ca
Nelson, Robert <rnelson0@gmail.com>
2014-04-20 23:00:26 +0000

    Add mcollective class to puppetmaster node definition

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,4 +10,15 @@ node 'puppet.nelson.va' {
   package { ['hiera', 'hiera-puppet']:
     ensure => present,
   }
+
+  class { '::mcollective':
+    middleware         => true,
+    middleware_hosts   => [ 'puppet.nelson.va' ],
+    middleware_ssl     => true,
+    securityprovider   => 'ssl',
+    ssl_client_certs   => 'puppet:///modules/site_mcollective/client_certs',
+    ssl_ca_cert        => 'puppet:///modules/site_mcollective/certs/ca.pem',
+    ssl_server_public  => 'puppet:///modules/site_mcollective/certs/puppet.nelson.va.pem',
+    ssl_server_private => 'puppet:///modules/site_mcollective/private_keys/puppet.nelson.va/pem',
+  }
 }
