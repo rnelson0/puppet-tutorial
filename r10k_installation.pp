a2db470d492dcc1ec70fcf5fd04a8448a4e39d82
Rob Nelson <rnelson0@gmail.com>
2014-12-30 13:10:22 +0000

    Add r10k_installation to revision control.

---
+++ r10k_installation.pp
@@ -0,0 +1,16 @@
+class { 'r10k':
+  version => '1.4.0',
+  sources => {
+    'puppet' => {
+      'remote'  => 'git://github.com/rnelson0/puppet-tutorial.git',
+      'basedir' => "${::settings::confdir}/environments",
+      'prefix'  => false,
+    },
+    'hiera' => {
+      'remote'  => 'git@github.com:rnelson0/hiera_home.git',
+      'basedir' => "/etc/puppet/hiera",
+      'prefix'  => false,
+    }
+  },
+  manage_modulepath => false
+}
